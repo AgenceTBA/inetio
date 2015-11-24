26dc2f1a68fc5ccb9311c8259d18b63b62530493
Baptiste Boullenger <wirder@gmail.com>
2015-11-24 18:55:40 +0100

    Add signup call

---
+++ application_mobile/www/js/signup.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('app')
+  .controller('SignupCtrl', function($scope, Auth, $state) {
+    $scope.form = {};
+    $scope.errors = {};
+    $scope.form.sexe = 'homme'
+    $scope.register = function(form) {
+
+      console.log(form);
+      $scope.submitted = true;
+
+      if (form.$valid) {
+        Auth.createUser({
+          nom: form.nom ,
+          prenom: form.prenom,
+          date: start,
+          sexe: form.sexe,
+          email: form.email,
+          password: form.password
+        })
+        .then(function() {
+          // Account created, redirect to home
+          $state.go('login');
+        })
+        .catch(function(err) {
+          err = err.data;
+          $scope.errors = {};
+
+          // Update validity of form fields that match the mongoose errors
+          angular.forEach(err.errors, function(error, field) {
+            form[field].$setValidity('mongoose', false);
+            $scope.errors[field] = error.message;
+          });
+        });
+      }
+    };
+
+
+
+
+  });
